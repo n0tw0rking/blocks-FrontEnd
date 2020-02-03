d6cd9db89beff2724a8b63aa9e14a92e20d828c8
AdamMomen <adammuman81@gmail.com>
2020-02-03 19:59:33 +0100

    Revert "changed the cube color to red"
    This reverts commit 69e60dc22861cbc5c3015a7017a77f69a6209be5.

--- src/app/modules/main-page/main-page.component.ts
+++ src/app/modules/main-page/main-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import * as THREE from "three";
-import * as GSAP from "gsap";
 @Component({
   selector: "app-main-page",
   templateUrl: "./main-page.component.html",
@@ -12,9 +11,14 @@ export class MainPageComponent implements OnInit {
   renderer = new THREE.WebGLRenderer({ antialias: true });
   mouse = new THREE.Vector2(0, 0);
   raycaster = new THREE.Raycaster();
+  geometry = new THREE.BoxGeometry(200, 200, 200);
+  material = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true
+  });
   scene = null;
   camera = null;
-  mesh = [];
+  mesh;
   light = null;
   light1 = null;
   intersects = null;
@@ -26,21 +30,20 @@ export class MainPageComponent implements OnInit {
     this.camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
-      0.1,
+      1,
       10000
     );
+    this.camera.position.z = 1000;
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
 
-    this.renderer.setClearColor("#e5e5e5");
-    this.camera.position.z = 5;
-
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+    this.geometry = new THREE.BoxGeometry(1, 1, 1);
+    this.material = new THREE.MeshLambertMaterial({ color: 0xff0000 });
     // new THREE.MeshBasicMaterial({
     //   color: 0xff0000,
     //   wireframe: true
     // });
     for (let i = 0; i < 9; i++) {
-      this.mesh[i] = new THREE.Mesh(geometry, material);
+      this.mesh[i] = new THREE.Mesh(this.geometry, this.material);
       this.mesh[i].position.x = (Math.random() - 0.5) * 10;
       this.mesh[i].position.y = (Math.random() - 0.5) * 10;
       this.mesh[i].position.z = (Math.random() - 0.5) * 10;
@@ -52,21 +55,12 @@ export class MainPageComponent implements OnInit {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
     this.animate();
-    this.light = new THREE.PointLight(0xffffff, 1, 1000);
-    this.light.position.set(0, 0, 0);
-    this.scene.add(this.light);
-
-    this.light1 = new THREE.PointLight(0xffffff, 2, 1000);
-    this.light1.position.set(0, 0, 25);
-    this.scene.add(this.light1);
   }
 
   animate() {
     window.requestAnimationFrame(() => this.animate());
-    for (var i = 0; i < 9; i++) {
-      this.mesh[i].rotation.x += 0.0125;
-      this.mesh[i].rotation.y += 0.002;
-    }
+    this.mesh.rotation.x += 0.01;
+    this.mesh.rotation.y += 0.02;
     this.renderer.render(this.scene, this.camera);
   }
   ngOnInit() {
@@ -76,33 +70,34 @@ export class MainPageComponent implements OnInit {
       this.camera.updateProjectMatrix();
     });
     // function that I will do make the boxes shake
-    window.addEventListener("mouseover", this.mouseover);
+    // window.addEventListener("mouseover", this.mouseover);
   }
-  mouseover(event) {
-    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    this.intersects = this.raycaster.setFromCamera(this.mouse, this.camera);
-    for (let i = 0; i < this.intersects.length; i++) {
-      this.tl = new GSAP.TimelineMax();
-      console.log("here");
-      this.tl.to(this.intersects[i].object.scale, 1, {
-        x: 2,
-        ease: Expo.easeOut
-      });
-      this.tl.to(this.intersects[i].object.scale, 0.5, {
-        x: 0.5,
-        ease: Expo.easeOut
-      });
-      this.tl.to(this.intersects[i].object.position, 0.5, {
-        x: 2,
-        ease: Expo.easeOut
-      });
-      this.tl.to(
-        this.intersects[i].object.rotation,
-        0.5,
-        { y: Math.PI * 0.5, ease: Expo.easeOut },
-        "=-1.5"
-      );
-    }
-  }
+  //   mouseover(event) {
+  //     this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  //     this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  //     this.intersects = this.raycaster.setFromCamera(this.mouse, this.camera);
+  //     for (let i = 0; i < this.intersects.length; i++) {
+  //       this.tl = new GSAP.TimelineMax();
+  //       console.log("here");
+  //       this.tl.to(this.intersects[i].object.scale, 1, {
+  //         x: 2,
+  //         ease: Expo.easeOut
+  //       });
+  //       this.tl.to(this.intersects[i].object.scale, 0.5, {
+  //         x: 0.5,
+  //         ease: Expo.easeOut
+  //       });
+  //       this.tl.to(this.intersects[i].object.position, 0.5, {
+  //         x: 2,
+  //         ease: Expo.easeOut
+  //       });
+  //       this.tl.to(
+  //         this.intersects[i].object.rotation,
+  //         0.5,
+  //         { y: Math.PI * 0.5, ease: Expo.easeOut },
+  //         "=-1.5"
+  //       );
+  //     }
+  //   }
+  // }
 }
