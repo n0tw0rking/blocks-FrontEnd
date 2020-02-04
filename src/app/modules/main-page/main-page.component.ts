68e1332846f5132ed6a23ed09cda09e4234f3828
AdamMomen <adammuman81@gmail.com>
2020-02-04 11:34:06 +0100

    in progress on fixing onMouseover

--- src/app/modules/main-page/main-page.component.ts
+++ src/app/modules/main-page/main-page.component.ts
@@ -6,7 +6,7 @@ import {
   HostListener
 } from "@angular/core";
 import * as THREE from "three";
-import * as GSAP from "gsap";
+import GSAP from "gsap";
 @Component({
   selector: "app-main-page",
   templateUrl: "./main-page.component.html",
@@ -36,12 +36,13 @@ export class MainPageComponent implements OnInit {
       1000
     );
     this.camera.position.z = 5;
-
+    this.raycaster = new THREE.Raycaster();
     this.geometry = new THREE.BoxGeometry(1, 1, 1);
     this.material = new THREE.MeshBasicMaterial({
       color: 0xf7f7f7
     });
     this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.mouse = new THREE.Vector2();
     this.light = new THREE.PointLight(0xff0000, 1, 100);
     this.light.position.set(0, 0, 100);
     this.scene.add(this.light);
@@ -56,64 +57,66 @@ export class MainPageComponent implements OnInit {
       this.mesh[i].position.x = (Math.random() - 0.5) * 10;
       this.mesh[i].position.y = (Math.random() - 0.5) * 10;
       this.mesh[i].position.z = (Math.random() - 0.5) * 10;
+      this.light = new THREE.PointLight(0xff0000, 1, 100);
+      this.light.position.set(0, 0, 100);
+      this.scene.add(this.light);
       this.scene.add(this.mesh[i]);
     }
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
     this.renderer.setClearColor("#e5e5e5");
-    this.light = new THREE.PointLight(0xff0000, 1, 100);
-    this.light.position.set(0, 0, 100);
-    this.scene.add(this.light);
-    this.light1 = new THREE.PointLight(0xff0000, 3, 100);
-    this.light1.position.set(0, 0, 100);
-    this.scene.add(this.light1);
     this.animate();
   }
 
   animate() {
-    window.requestAnimationFrame(() => this.animate());
-    for (let i = 0; i < 9; i++) {
-      this.mesh[i].rotation.x += 0.00155;
-      this.mesh[i].rotation.y += 0.001;
-      this.renderer.render(this.scene, this.camera);
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    this.intersects = this.raycaster.intersectObjects(this.scene.children);
+    console.log(this.intersects);
+    for (let i = 0; i < this.intersects.length; i++) {
+      this.intersects[i].object.material.color.set(0xff0000);
     }
+    for (let i = 0; i < 9; i++) {
+      this.mesh[i].rotation.x += 0.001;
+      this.mesh[i].rotation.y += 0.001;
+      this.mesh[i].rotation.z += 0.001;
+    }
+
+    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(() => this.animate());
   }
   ngOnInit() {}
   @HostListener("window:resize", ["$event"])
   onWindowResize(event) {
     this.renderer.setSize(event.target.innerWidth, event.target.innerHeight);
   }
-  @HostListener("window:click", ["$event"])
-  onWindowClick(event) {
-    this.click(event);
-  }
   click(event) {
-    this.raycaster = new THREE.Raycaster();
-    this.mouse = new THREE.Vector2();
+    // this.tl = GSAP.timeline();
+    // for (let i = 0; i < this.intersects.length; i++) {
+    //   this.intersects[i].object.material.color.set(0xff0000);
+    // this.tl.to(this.intersects[i].object.scale, 1, {
+    //   x: 2,
+    //   ease: Expo.easeOut
+    // });
+    // this.tl.to(this.intersects[i].object.scale, 0.5, {
+    //   x: 0.5,
+    //   ease: Expo.easeOut
+    // });
+    // this.tl.to(this.intersects[i].object.position, 0.5, {
+    //   x: 2,
+    //   ease: Expo.easeOut
+    // });
+    // this.tl.to(
+    //   this.intersects[i].object.rotation,
+    //   0.5,
+    //   { y: Math.PI * 0.5, ease: Expo.easeOut },
+    //   "=-1.5"
+    // );
+    // }
+    // this.renderer.render(this.scene, this.camera);
+  }
+  @HostListener("window:mouseover", ["$event"])
+  onMouseOver(event) {
     this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    this.intersects = this.raycaster.setFromCamera(this.mouse, this.camera);
-    console.log(this.intersects);
-    for (let i = 0; i < this.intersects.length; i++) {
-      this.tl = new GSAP.TimelineMax();
-      this.tl.to(this.intersects[i].object.scale, 1, {
-        x: 2,
-        ease: Expo.easeOut
-      });
-      this.tl.to(this.intersects[i].object.scale, 0.5, {
-        x: 0.5,
-        ease: Expo.easeOut
-      });
-      this.tl.to(this.intersects[i].object.position, 0.5, {
-        x: 2,
-        ease: Expo.easeOut
-      });
-      this.tl.to(
-        this.intersects[i].object.rotation,
-        0.5,
-        { y: Math.PI * 0.5, ease: Expo.easeOut },
-        "=-1.5"
-      );
-    }
   }
 }
