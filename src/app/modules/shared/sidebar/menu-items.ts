5de7d4a50919cada69d471fbf9d42444fe4ec108
AdamMomen <adammuman81@gmail.com>
2020-02-05 20:59:21 +0100

    completed the basic home completed

---
+++ src/app/modules/shared/sidebar/menu-items.ts
@@ -0,0 +1,44 @@
+import { RouteInfo } from "./sidebar.metadata";
+
+export const ROUTES: RouteInfo[] = [
+  {
+    path: "/service",
+    title: "Services",
+    icon: "mdi mdi-image-filter-vintage",
+    class: "",
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: "/component/progressbar",
+    title: "Progressbar",
+    icon: "mdi mdi-poll",
+    class: "",
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: "/component/rating",
+    title: "Blocks",
+    icon: "mdi mdi-bandcamp",
+    class: "",
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: "/component/tabs",
+    title: "Tabs",
+    icon: "mdi mdi-sort-variant",
+    class: "",
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: "/component/buttons",
+    title: "TEST",
+    icon: "mdi mdi-toggle-switch",
+    class: "",
+    extralink: false,
+    submenu: []
+  }
+];
