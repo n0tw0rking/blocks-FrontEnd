d019aee30feaa6873284d00a385af28cc7e07336
Omar Bara <omarbara@ymail.com>
2020-02-05 15:56:12 +0100

    capture Parm routing for user by block id

--- src/app/modules/main-page/user/users.component.ts
+++ src/app/modules/main-page/user/users.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -7,16 +7,22 @@ import { Router } from '@angular/router';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
 users = [
   {email:"one@one.com", userSubscription: [{name:'fathi1',balance:-200,user:'username1',block:'block1'}] ,  adminBlock:'admin1'},
   {email:"tow@tow.com", userSubscription: [{name:'hasan2',balance:100,user:'username2',block:'block2'}] ,  adminBlock:'admin2'}
 
 ]
-  constructor( private router: Router) { }
-
-  ngOnInit() {
-  }
+  constructor( private router: Router, private route: ActivatedRoute) { }
+id='';
+sub:any;
+ngOnInit() {
+    this.sub = this.route.params.subscribe(params =>{
+      // this.id = params+'';
+    //  this.getBlock(this.id)
+    console.log(params)
+    })
+ }
   // onSelect(use) {
   //   console.log(use)
 
@@ -24,9 +30,13 @@ users = [
   userMoreInfo(use){
     console.log(use)
     //redirect to new page that have rteh user record
-    this.router.navigate(["/user/balance"]);
+    this.router.navigate(["/user/balance", use.email]);
       // Get http record
 
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
