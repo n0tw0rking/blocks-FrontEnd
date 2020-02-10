28901b360282431f68d61f319493d95a0dd259f2
AdamMomen <adammuman81@gmail.com>
2020-02-10 23:08:14 +0100

    added enabling and disabling functionality of service

--- src/app/core/apollo.service.ts
+++ src/app/core/apollo.service.ts
@@ -40,7 +40,7 @@ export class ApolloService {
       query: gql`
         {
           services {
-            serviceName
+            aServiceId
             serviceName
             isActive
           }
@@ -141,15 +141,22 @@ export class ApolloService {
       errorPolicy: "all"
     }).valueChanges;
   }
-  updateServiceById(serviceId) {
-    return this.apollo.use("ASP").watchQuery<any>({
-      query: gql`
-        query($blockId: Int!) {
-        
-         } `,
-      variables: {},
+  updateServiceById(serviceId, state) {
+    return this.apollo.use("ASP").mutate<any>({
+      mutation: gql`
+        mutation($inputServiceId: Int!, $serviceState: Boolean!) {
+          updateServiceState(
+            inputServiceId: $inputServiceId
+            serviceState: $serviceState
+          )
+        }
+      `,
+      variables: {
+        inputServiceId: serviceId,
+        serviceState: state
+      },
       errorPolicy: "all"
-    }).valueChanges;
+    });
   }
   getBlocksByAdminId(id) {
     //get all info of block by id
@@ -188,40 +195,42 @@ export class ApolloService {
             }
           }
         }
-        
-       `,
-       errorPolicy: "all"
-     })
-     .valueChanges
-   
- 
-   }
- 
-   getUsersOfBlock(block) { //get all user s inside this block(id)
+      `,
+      errorPolicy: "all"
+    }).valueChanges;
+  }
+
+  getUsersOfBlock(block) {
+    //get all user s inside this block(id)
     // console.log(typeof(+block), "inside apolo")
-    return this.apollo.use("ASP")
-    .watchQuery<any>({
-        query: gql`
-        query($blockId: Int!){
-          block(blockId:$blockId) {
-          blockSubscriptions{subscriptionId,subscription{user{email, userId, phoneNumber }}}}
+    return this.apollo.use("ASP").watchQuery<any>({
+      query: gql`
+        query($blockId: Int!) {
+          block(blockId: $blockId) {
+            blockSubscriptions {
+              subscriptionId
+              subscription {
+                user {
+                  email
+                  userId
+                  phoneNumber
+                }
+              }
+            }
+          }
         }
-       `,
-       variables: {
+      `,
+      variables: {
         blockId: +block
       },
-       errorPolicy: "all"
-     })
-     .valueChanges
-   }
- 
- 
- //works
-   createNewBlock(Block) {
-      console.log(Block, 'inside newblock')
-    return this.apollo
-    .mutate<any>({
+      errorPolicy: "all"
+    }).valueChanges;
+  }
 
+  //works
+  createNewBlock(Block) {
+    console.log(Block, "inside newblock");
+    return this.apollo.mutate<any>({
       mutation: gql`
         mutation($name: String!, $location: String!) {
           createBlock(blockInput: { name: $name, location: $location }) {
--- src/app/modules/services-page/service/service.component.ts
+++ src/app/modules/services-page/service/service.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { ApolloService } from "../../../core/apollo.service";
 @Component({
-  selector: 'app-service',
-  templateUrl: './service.component.html',
-  styleUrls: ['./service.component.css']
+  selector: "app-service",
+  templateUrl: "./service.component.html",
+  styleUrls: ["./service.component.css"]
 })
 export class ServiceComponent implements OnInit {
-
-  constructor() { }
-  @Input() service:any
+  loading = false;
+  error;
+  constructor(private apollo: ApolloService) {}
+  @Input() service: any;
 
   ngOnInit() {
+    console.log(this.service);
   }
-
+  updateService(serviceid, state) {
+    console.log(serviceid, state);
+    this.apollo.updateServiceById(serviceid, state).subscribe(
+      result => {
+        if (result.errors) {
+          this.loading = false;
+          console.log(result.errors[0].message);
+        } else {
+          // this.service = result.data.services;
+          console.log(result);
+          this.loading = result.data.loading;
+        }
+      },
+      errorResponse => {
+        console.log(errorResponse);
+      }
+    );
+  }
 }
