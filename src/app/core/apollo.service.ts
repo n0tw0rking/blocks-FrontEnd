76bc3bcca09d62780f86512e3c9e61bba1ceecab
weeehabla <weeehaplay@gmail.com>
2020-02-11 11:51:02 +0100

    nothing speacial

--- src/app/core/apollo.service.ts
+++ src/app/core/apollo.service.ts
@@ -7,4 +7,92 @@ import gql from "graphql-tag";
 })
 export class ApolloService {
   constructor(private apollo: Apollo) {}
+
+  getUser(currentUser): any {
+    return this.apollo.watchQuery<any>({
+      query: gql`
+        query($id: String!) {
+          oneUser(id: $id) {
+            _id
+            isAdmin
+            isSuperAdmin
+            email
+            password
+            userSubscription {
+              _id
+              name
+              user {
+                _id
+              }
+              block {
+                _id
+                name
+                location
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        id: currentUser
+      },
+      errorPolicy: "all"
+    }).valueChanges;
+  }
+
+  getService(serviceNAme): any {
+    return this.apollo.watchQuery<any>({
+      query: gql`
+        query($name: String!) {
+          oneService(name: $name) {
+            _id
+            subscriptionId {
+              _id
+            }
+          }
+        }
+      `,
+      variables: {
+        name: serviceNAme
+      },
+      errorPolicy: "all"
+    }).valueChanges;
+  }
+
+  getSubscription(subName): any {
+    return this.apollo.watchQuery<any>({
+      query: gql`
+        query($name: String!) {
+          oneSubscription(name: $name) {
+            _id
+
+            block {
+              _id
+            }
+          }
+        }
+      `,
+      variables: {
+        name: subName
+      },
+      errorPolicy: "all"
+    }).valueChanges;
+  }
+
+  getBlockSubs(blockName) {
+    return this.apollo.watchQuery<any>({
+      query: gql`
+        query($name: String!) {
+          oneBlockSubs(name: $name) {
+            _id
+            name
+          }
+        }
+      `,
+      variables: {
+        name: blockName
+      },
+      errorPolicy: "all"
+    }).valueChanges;
+  }
 }
