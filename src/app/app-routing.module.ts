3739b7d1d2f7129acfa6588e5495c4997323c952
Omar Bara <omarbara@ymail.com>
2020-02-15 11:36:41 +0100

    route modify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,23 +4,16 @@ import { UsersComponent } from "./modules/main-page/user/users.component";
 import { LoginComponent } from "./modules/login/login.component";
 import { MainPageComponent } from "./modules/main-page/main-page.component";
 import { ServicesPageComponent } from "./modules/services-page/services-page.component";
-import { BlocksPageComponent } from "./modules/blocks-page/blocks-page.component";
 import { AuthGuard } from "./core/auth.guard";
-import { BlockComponent } from "./modules/blocks-page/block/block.component";
 import { SuperAdminComponent } from "./modules/super-admin/super-admin.component";
 import { BalanceEditComponent } from "./modules/main-page/user/balance-edit/balance-edit.component";
 import { ErrorPageComponent } from "./modules/error-page/error-page.component";
-import { DashBoardComponent } from "./modules/dash-board/dash-board.component";
-import { SubscriptionComponent } from "./modules/subscription/subscription.component";
+
 const routes: Routes = [
-  {
-    path: "super",
-    component: SuperAdminComponent
-    // canActivate: [AuthGuard]
-  },
   { path: "login", component: LoginComponent },
   { path: "", component: MainPageComponent },
   // { path:'user', component: UserComponent,  canActivate:[AuthGuard] ,
+  // { path: "user/add", component: AddUserComponent },
   { path: "user/balance/:id", component: BalanceEditComponent },
   { path: "user/:id", component: UsersComponent },
 
@@ -30,7 +23,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: "", component: UsersComponent }
-      //   // { path: ":id", component: UsersComponent }
+    //   // { path: ":id", component: UsersComponent }
     ]
   },
   {
@@ -49,21 +42,18 @@ I have added the name so I can search by name
     // canActivate: [AuthGuard]
   },
   // { path: "blocks", component: BlocksPageComponent },
-  {
-    path: "blocks",
-    component: BlocksPageComponent,
-    // canActivate: [AuthGuard],
-    children: [
-      { path: "", component: BlockComponent }
-      // {path: 'edit/:id', component: EditComponent}
-    ]
-  },
-  { path: "dash", component: DashBoardComponent, canActivate: [AuthGuard] },
-  {
-    path: "subscription",
-    component: SubscriptionComponent,
-    canActivate: [AuthGuard]
-  }
+  // {
+  //   path: "blocks",
+  //   component: BlocksPageComponent,
+  //   // canActivate: [AuthGuard],
+  //   children: [
+  //     { path: "", component: BlockComponent }
+  //     // {path: 'edit/:id', component: EditComponent}
+  //   ]
+  // },
+  // { path: "blocks/add", component: AddBlockComponent },
+  // { path: "blocks/edit/:name", component: EditComponent }
+
   // { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
   // { path: '**', redirectTo: '/not-found' }
 ];
