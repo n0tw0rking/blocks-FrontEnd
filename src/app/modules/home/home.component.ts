fadb24e20befcbe1a6fd4e0fd23a245098ca5ec0
AdamMomen <adammuman81@gmail.com>
2020-02-16 17:07:30 +0100

    issue w/ webpack

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -13,47 +13,69 @@ import * as THREE from "three-full";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent implements OnInit {
   @ViewChild("rendererContainer", { static: false })
   rendererContainer: ElementRef;
-  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer;
   material = null;
   scene = null;
   mesh = null;
   camera = null;
   geometry = null
+  textureCube = null
+  cube;
   constructor() {
+    // create a temp holder to save the canvas outside of canvas
+    var tempHolder = document.createElement('div');
+    tempHolder.style.display = 'none';
+    window.document.body.appendChild(tempHolder);
+
+
+    this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
-    ); this.mesh = new THREE.Mesh(this.geometry, this.material);
-    this.camera.position.z = 5;
+    );
+    this.renderer = new THREE.WebGLRenderer();
+    this.camera.position.z = 15;
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+    console.log(this.rendererContainer)
+    // this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
     this.geometry = new THREE.BoxGeometry(1, 1, 1);
     this.material = new THREE.MeshBasicMaterial({
-      color: 0xf7f7f7
+      color: 0xf7f7f7,
+      // envMap: textureCube 
     });
-    this.scene.add(this.mesh);
-  }
+    this.cube = new THREE.Mesh(this.geometry, this.material);
 
+
+
+    this.scene.add(this.cube);
+
+  }
+  ngOnInit() { }
   ngAfterViewInit() {
+    this.cube.position.x = -10
+    this.cube.position.y = -6
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
-    this.renderer.setClearColor("#e5e5e5");
-    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+    // this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
     this.animate();
   }
   animate() {
     window.requestAnimationFrame(() => this.animate());
+    this.cube.position.x = this.camera.position.x - 10
+    this.cube.position.y = this.camera.position.y - 10
+    this.cube.rotation.y += 0.01;
     this.renderer.render(this.scene, this.camera);
-    // this.raycaster.setFromCamera(this.mouse, this.camera);
   }
   @HostListener("window:resize", ["$event"])
   onWindowResize(event) {
+    console.log
     this.renderer.setSize(event.target.innerWidth, event.target.innerHeight);
     this.camera.aspect = window.innerWidth / window.innerHeight;
-
     this.camera.updateProjectionMatrix();
   }
 }
